d076672c1c48500ff2f316ca32a9136fd570c181
leonardas103 <wingdragon_9@hotmail.com>
2018-04-16 12:08:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import config
 import BCOSFIRE_media
 
 def main():
-    symm_params = config
-    symm_params.inputfilter.DoG.sigmalist   = 2.4
-    symm_params.COSFIRE.rholist             = [0, 2, 4, 6, 8]
-    symm_params.COSFIRE.sigma0              = 3
-    symm_params.COSFIRE.alpha               = 0.7
+    symmfilter = config
+    symmfilter.inputfilter.DoG.sigmalist   = 2.4
+    symmfilter.COSFIRE.rholist             = [0, 2, 4, 6, 8]
+    symmfilter.COSFIRE.sigma0              = 3
+    symmfilter.COSFIRE.alpha               = 0.7
 
-    asymm_params = config
-    asymm_params.inputfilter.DoG.sigmalist   = 1.8
-    asymm_params.COSFIRE.rholist             = [i for i in range(0,22+1,2)]
-    asymm_params.COSFIRE.sigma0              = 2
-    asymm_params.COSFIRE.alpha               = 0.1
+    asymmfilter = config
+    asymmfilter.inputfilter.DoG.sigmalist   = 1.8
+    asymmfilter.COSFIRE.rholist             = [i for i in range(0,22+1,2)]
+    asymmfilter.COSFIRE.sigma0              = 2
+    asymmfilter.COSFIRE.alpha               = 0.1
 
     image_path = 'data/RETINA_example/01_test.tif'
-    BCOSFIRE_media.main(image_path, symm_params, asymm_params, 0.5)
+    BCOSFIRE_media.main(image_path, symmfilter, asymmfilter, 0.5)
 
 
 if __name__ == "__main__":
