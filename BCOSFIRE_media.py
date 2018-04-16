e0a58a07d85bedb86cd66680ea58a3e4727aa234
leonardas103 <wingdragon_9@hotmail.com>
2018-04-16 12:09:24 +0200

    Update BCOSFIRE_media.py

--- BCOSFIRE_media.py
+++ BCOSFIRE_media.py
@@ -6,6 +6,16 @@ def load_image(path):
     height, width = img.shape
     return (img, height, width)
 
+def make_prototype(length):
+    a = np.zeros((length,length//2), dtype=int)
+    b = np.ones((length,1),  dtype=int)
+    return np.concatenate((a,b,a), axis=1)
+
 def main(image, filter1, filter2, preprocessthresh):
-    x,y = 101,101
-    np.zeros((201,201), dtype=int)
+    x,y = 100,100
+    numoriens = 12
+    prototype = make_prototype(201)
+    symmfilter = (prototype, [x,y], filter1)
+    numoriens = 24 #why do we change it here?
+    filter2.set_rotation_psilist(filter2, numoriens, 2)
+    asymmfilter = (prototype, [x,y], filter1)
